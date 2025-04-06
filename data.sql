dc299827277c4a54ee9ff744047fa573e937ee26
Pradeepa ARUMUGAM <130929097+deeepzz@users.noreply.github.com>
2025-04-07 01:59:32 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -5,27 +5,8 @@ CREATE TABLE spotify (
     track VARCHAR(255),
     album VARCHAR(255),
     album_type VARCHAR(50),
-    danceability FLOAT,
-    energy FLOAT,
-    loudness FLOAT,
-    speechiness FLOAT,
-    acousticness FLOAT,
-    instrumentalness FLOAT,
-    liveness FLOAT,
-    valence FLOAT,
-    tempo FLOAT,
-    duration_min FLOAT,
     title VARCHAR(255),
-    channel VARCHAR(255),
-    views FLOAT,
-    likes BIGINT,
-    comments BIGINT,
-    licensed BOOLEAN,
-    official_video BOOLEAN,
-    stream BIGINT,
-    energy_liveness FLOAT,
-    most_played_on VARCHAR(50)
-);
+    );
 
 -------------------EDA-------------------
 
@@ -42,79 +23,40 @@ select count(distinct album) from spotify;
 select distinct album_type from spotify;
 
 --5. find out maximum,minimum duration ?
-select max(duration_min) from spotify;
-select min(duration_min) from spotify;
+-- Removed due to missing duration data
 
 --6. Delete the song where duration is zero.
-delete from spotify
-where duration_min = 0;
+-- Removed due to missing duration data
 
 -----------------------Easy Category-------------------
 
---1.Retrieve the names of all tracks that have more than 1 billion streams
-select track from spotify
-where stream > 1000000000;
-
---2.List all albums along with their respective artists.
+--1.List all albums along with their respective artists.
 select album,artist
 from spotify;
 
---3.Get the total number of comments for tracks where licensed = TRUE.
-select Sum(comments) as total_comment
-from spotify
-where licensed = True;
-
---4.Find all tracks that belong to the album type single.
-select track from spotify
-where album_type = 'single';
-
---5.Count the total number of tracks by each artist.
+--2.Count the total number of tracks by each artist.
 select count(track) as total_no_counts,artist from spotify
 group by artist
-order by total_no_counts desc
+order by total_no_counts desc;
+
+--3. Lister les 10 morceaux les plus populaires
+select track, popularity
+from spotify
+order by popularity desc
+limit 10;
+
+--4. Lister tous les morceaux d’un artiste spécifique (ex: TV Girl)
+select track
+from spotify
+where artist = 'TV Girl';
 
 ------------------------Medium level-------------------
 
---1.Calculate the average danceability of tracks in each album.
-select album, avg(danceability) as avg_danceability from spotify
-group by album
-order by avg_danceability desc
-
---2.Find the top 5 tracks with the highest energy values.
-select distinct track,energy from spotify
-order by energy desc
-limit 5
-
---3.List all tracks along with their views and likes where official_video = TRUE
-select track,sum(views) as total_views,sum(likes) as total_likes
+--3.Répartition des morceaux par année de sortie (en supposant que le champ title contient l'année ou une colonne 'release_year' existe)
+-- Exemple avec colonne 'release_year'
+select strftime('%Y', release_date) as year, count(*) as total_tracks
 from spotify
-where official_video = 'TRUE'
-group by track
-order by total_views desc;
-
---4.For each album, calculate the total views of all associated tracks.
-select album,track,sum(views) as total_views
-from spotify
-group by album,track
-order by total_views desc
-
---5.Retrieve the track names that have been streamed on Spotify more than YouTube.
-select * from(select track,
-coalesce(sum(case when most_played_on = 'Youtube' then stream END),0) as streamed_on_youtube,
-coalesce(sum(case when most_played_on = 'Spotify' then stream END),0) as streamed_on_Spotify
-from spotify
-group by track) as t 
-where streamed_on_Spotify > streamed_on_youtube
-and streamed_on_youtube <> 0
-
---6.Find the top 3 most-viewed tracks for each artist using window functions?
-with cte as (select artist,track,
-sum(views) as total_views,
-dense_rank() over(partition by artist order by sum(views) desc) as row_num
-from spotify
-group by artist ,track
-order by 1,3 desc)
-select artist,track,row_num
-from cte
-where row_num < 4;
+where release_date is not null
+group by year
+order by year;
 
